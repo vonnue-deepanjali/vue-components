8e03dd20d71ee666743876e8accc236e96445684
vonnue-deepanjali <deepanjali.dilish@vonnue.com>
2025-07-22 10:35:31 +0530

    refactor: Separator Component added the thickness and length as props

--- src/Components/Separator/Separator.vue
+++ src/Components/Separator/Separator.vue
@@ -1,16 +1,15 @@
 <template>
   <v-divider
     :vertical="vertical"
-    :style="dividerStyle"
-    class="custom-divider"
-    role="separator"
+    :thickness="thickness"
+    :length="length"
     :aria-orientation="vertical ? 'vertical' : 'horizontal'"
+    role="separator"
+    class="custom-divider"
   />
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
-
 import type { SeparatorProps } from '@/type/Separator'
 
 const props = withDefaults(defineProps<SeparatorProps>(), {
@@ -19,31 +18,10 @@ const props = withDefaults(defineProps<SeparatorProps>(), {
   color: '#000000',
   vertical: true,
 })
-
-const createSeparatorStyle = ({ thickness, length, color, vertical = true }: SeparatorProps) => {
-  if (vertical) {
-    return {
-      width: typeof thickness === 'number' ? `${thickness}px` : thickness,
-      height: typeof length === 'number' ? `${length}px` : length,
-      backgroundColor: color,
-      display: 'inline-block',
-    }
-  } else {
-    return {
-      width: typeof length === 'number' ? `${length}px` : length,
-      height: typeof thickness === 'number' ? `${thickness}px` : thickness,
-      backgroundColor: color,
-      display: 'inline-block',
-    }
-  }
-}
-
-const dividerStyle = computed(() => createSeparatorStyle(props))
 </script>
 
 <style scoped>
 .custom-divider {
   margin: 0 8px;
-  display: inline-block;
 }
 </style>
